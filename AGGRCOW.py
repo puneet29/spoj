9274cfb7898a1f76eacd7a5efcce10a5150cd340
Puneet Saini <puneet29saini@gmail.com>
2019-04-07 00:43:16 +0530

    Made AGGRCOW more clear

--- AGGRCOW.py
+++ AGGRCOW.py
@@ -21,10 +21,11 @@ for _ in range(T):
     end = X[N-1]
     start = 0
 
-    while(end>start):
-        mid = start + (end-start+1)//2
+    while(end>=start):
+        mid = (start + end)//2
         if(F(X, N, C, mid)):
-            start = mid
+            start = mid + 1
+            ans = mid
         else:
             end = mid - 1
-    print(start)
+    print(ans)
