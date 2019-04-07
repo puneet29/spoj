bb46a9733ed08b42048285ad5c410811281d4559
Puneet Saini <puneet29saini@gmail.com>
2019-04-07 12:24:17 +0530

    Removed extra loop [PRIME1]

--- PRIME1.py
+++ PRIME1.py
@@ -26,12 +26,12 @@ for _ in range(T):
         if(S[i]):
             if(not checkPrime(X[i])):
                 S[i] = False
+            else:
+                if(i >= l and i <= u):
+                    print(X[i])
             if(X[i] <= 1):
                 continue
             for j in range(i+X[i], u+1, X[i]):
                 S[j] = False
 
-    for i in range(l, u+1):
-        if(S[i]):
-            print(X[i])
     print()
