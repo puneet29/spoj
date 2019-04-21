b69b8e46535ebcbe3ed24421d6a6314962df006a
Puneet Saini <puneet29saini@gmail.com>
2019-04-21 22:29:52 +0530

    Updated ACODE. Bottom up approach. AC

--- ACODE.py
+++ ACODE.py
@@ -1,19 +1,23 @@
-def decode(S, i, j, memo):
-    if(j == i-1):
-        return 1
-    if(i in memo):
-        return(memo[i])
-    else:
-        ans = decode(S, i+1, j, memo)
-        if(j-i >= 1):
+def decode(S, N):
+    memo = [0 for i in range(N+1)]
+    memo[N-1] = 1
+    memo[N] = 1 if S[-1] != "0" else 0
+    for i in reversed(range(N-1)):
+        if(S[i] != "0"):
             if(int(S[i:i+2]) <= 26):
-                ans += decode(S, i+2, j, memo)
-        memo[i] = ans
-        return (ans)
+                memo[i] = memo[i+1] + memo[i+2]
+            else:
+                memo[i] = memo[i+1]
+        else:
+            if(int(S[i-1]) == 2 or int(S[i-1]) == 1):
+                memo[i] = 0
+            else:
+                return(0)
+    return(memo[0])
 
 
 while(True):
     n = input()
     if(n == "0"):
         break
-    print(decode(n, 0, len(n)-1, {}))
+    print(decode(n, len(n)))
